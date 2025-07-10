8a322488623e6a70434b52b80f1b35b323042f14
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2025-07-10 18:32:44 -0300

    feat: add routes to create and show rooms

--- src/app.tsx
+++ src/app.tsx
@@ -1,3 +1,14 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { CreateRoom } from './pages/create-room';
+import { Room } from './pages/room';
+
 export function App() {
-  return <div>Hello World</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<CreateRoom />} index />
+        <Route element={<Room />} path="/room" />
+      </Routes>
+    </BrowserRouter>
+  );
 }
---
+++ src/pages/create-room.tsx
@@ -0,0 +1,11 @@
+import { Link } from 'react-router-dom';
+
+export function CreateRoom() {
+  return (
+    <div>
+      <div>Create Room</div>
+
+      <Link to="/room">Acessar sala</Link>
+    </div>
+  );
+}
