2adf739c0c14adccb7b50550a6fe1fa3d8f46fb9
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2025-07-10 22:43:25 -0300

    feat: add a basic content for create room and room pages

--- src/pages/create-room.tsx
+++ src/pages/create-room.tsx
@@ -1,11 +1,32 @@
+import { useQuery } from '@tanstack/react-query';
 import { Link } from 'react-router-dom';
 
+type GetRoomsAPIResponse = Array<{
+  id: string;
+  name: string;
+}>;
+
 export function CreateRoom() {
+  const { data, isLoading } = useQuery({
+    queryKey: ['get-rooms'],
+    queryFn: async () => {
+      const response = await fetch('http://localhost:3333/rooms');
+      const result: GetRoomsAPIResponse = await response.json();
+
+      return result;
+    }
+  });
+
   return (
     <div>
-      <div>Create Room</div>
-
-      <Link to="/room">Acessar sala</Link>
+      {isLoading && <p>Carregando...</p>}
+      <div className="flex flex-col gap-1">
+        {data?.map((room) => (
+          <Link key={room.id} to={`/room/${room.id}`}>
+            {room.name}
+          </Link>
+        ))}
+      </div>
     </div>
   );
 }
